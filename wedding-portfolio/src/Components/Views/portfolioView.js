af6ca1773a67e0cb9310a7929064d64a60b755b1
Laura Daugherty <lauradaugherty6@gmail.com>
2020-01-17 15:18:16 -0800

    code cleanup

--- wedding-portfolio/src/Components/Views/portfolioView.js
+++ wedding-portfolio/src/Components/Views/portfolioView.js
@@ -9,48 +9,49 @@ import { Margin } from "./styling";
 
 function PortfolioView() {
 
-const [portFolioView, setPortFolioView] = useState([])
+    const [portFolioView, setPortFolioView] = useState([])
 
-useEffect(() => {
-    axiosWithAuth()
-    .get("https://bw19-wedding-planner-portfolio.herokuapp.com/api/posts")
-    .then(look => {
-        setPortFolioView(look.data.posts)
-        })
+    useEffect(() => {
+        axiosWithAuth()
+        .get("https://bw19-wedding-planner-portfolio.herokuapp.com/api/posts")
+        .then(look => {
+                setPortFolioView(look.data.posts)
+            })
         .catch(error => {
-        console.log("ERROR", error)  
+                console.log("ERROR", error)  
             })
     }, [])
-if(!portFolioView || portFolioView.length === 0) {
-    return (
-        <section> 
-               {/* Webstie main content */}
-            <article> 
-                 <div> 
-                    <h3 style={{color: "#FA80AF"}}>
-                        Add Your First Post To Get Started!
-                    </h3>
-                 </div>
-            </article>
-        </section>
-    )
-} else {
-    return(
-        <Margin> 
-<section> 
-       {/* Webstie main content */}
-       <Margin> </Margin>
-    <article> 
-        <Card.Group className='cardGroup' doubling centered itemsPerRow={3}>
-           { portFolioView.map(watch => {
-               return <WeddingCard key={watch.id} watch={ watch } setPortFolioView={setPortFolioView} portFolioView={portFolioView} /> 
-           })} 
-        </Card.Group>
-    </article>
-</section>
-    </Margin>
-    ); 
-}
+
+    if(!portFolioView || portFolioView.length === 0) {
+        return (
+            <section> 
+                {/* Webstie main content */}
+                <article> 
+                    <div> 
+                        <h3 style={{color: "#FA80AF"}}>
+                            Add Your First Post To Get Started!
+                        </h3>
+                    </div>
+                </article>
+            </section>
+        )
+    } else {
+        return(
+            <Margin> 
+                <section> 
+                    {/* Webstie main content */}
+                    <Margin> </Margin>
+                    <article> 
+                        <Card.Group className='cardGroup' doubling centered itemsPerRow={3}>
+                            { portFolioView.map(watch => {
+                                return <WeddingCard key={watch.id} watch={ watch } setPortFolioView={setPortFolioView} portFolioView={portFolioView} /> 
+                            })} 
+                        </Card.Group>
+                    </article>
+                </section>
+            </Margin>
+        ); 
+    }
 }
 
 
